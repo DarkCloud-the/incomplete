7a070fa69c203ccb062079bc4a6debe328240562
The dark cloud <gusmitf@gmail.com>
2023-05-09 17:01:14 -0700

    Create for the back.c

---
+++ acld/for the back.c
@@ -0,0 +1,10 @@
+#include <math.h>
+#include <stdlib.h>
+#include <sys/errno.h>
+while INFINITY
+array.forEach(currentItem => {
+  
+})
+
+ENAMETOOLONG
+qsort(void *base, size_t nel, size_t width, int (* _Nonnull compar)(const void *, const void *))
